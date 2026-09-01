b8bc238c37b6d914ace244b1189eadc645727dfd
mdrade <marcelloandraderodrigues@gmail.com>
2026-09-01 19:45:20 -0300

    Aula-01/09

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,8 +7,8 @@ from app import app
 def test_health_check():
     client = app.test_client()
     response = client.get('/health')
-    assert response.status.code == 200
-    assert response.get_json()["Status"] == "ok"
+    assert response.status_code == 200
+    assert response.get_json()["status"] == "ok"
 
 def test_home():
     client = app.test_client()
